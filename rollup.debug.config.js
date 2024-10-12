3b3ff854f63c1996005c08289ca25bfa3f492273
Virgil Clyne <Virgil@nanocat.me>
2024-10-12 11:23:38 +0800

    chore: update-package-version-by-release-tag
    Update package-lock.json
    Update package.json
    Update build.yml

--- rollup.debug.config.js
+++ rollup.debug.config.js
@@ -1,3 +1,4 @@
+import pkg from './package.json' with { type: "json" };
 import json from '@rollup/plugin-json';
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from '@rollup/plugin-node-resolve';
@@ -8,7 +9,7 @@ export default [
 		output: {
 			file: 'dist/response.js',
 			//format: 'es',
-			banner: `/* README: https://github.com/NSRingo */\nconsole.log(' iRingo: 🌤 WeatherKit β Response')\nconsole.log('${new Date().toLocaleString('zh-CN', {timeZone: 'PRC'})}')`,
+			banner: `/* README: https://github.com/NSRingo */\nconsole.log(' iRingo: 🌤 WeatherKit β Response')\nconsole.log('Version: ${pkg.version}')\nconsole.log('Build Time: ${new Date().toLocaleString('zh-CN', {timeZone: 'PRC'})}')`,
 		},
 		plugins: [json(), commonjs(), nodeResolve()]
 	}
