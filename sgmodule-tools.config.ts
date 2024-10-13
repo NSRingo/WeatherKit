5c609abb8c56e730ad9c744cbb8f2753c470871e
baranwang <me@baran.wang>
2024-10-13 10:55:16 +0800

    feat: sgmoudle-tools

---
+++ sgmodule-tools.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "@iringo/sgmoudle-tools";
+import { ArgumentsBuilder } from "@iringo/arguments-builder";
+import argConfig from "./arguments-builder.config";
+
+const args = new ArgumentsBuilder(argConfig);
+
+const { scriptParams } = args.buildSurgeArguments();
+
+export default defineConfig({
+  module: {
+    rule: ["DOMAIN,weather-analytics-events.apple.com,REJECT-DROP"],
+    script: [
+      {
+        name: "🌤 WeatherKit.api.v1.availability.response",
+        type: "http-response",
+        scriptPath: "./dist/response.js",
+        pattern: "^https?://weatherkit.apple.com/api/v1/availability/",
+        requiresBody: true,
+        engine: "webview",
+        argument: scriptParams,
+      },
+      {
+        name: "🌤 WeatherKit.api.v2.weather.response",
+        type: "http-response",
+        scriptPath: "./dist/response.js",
+        pattern: "^https?://weatherkit.apple.com/api/v2/weather/",
+        requiresBody: true,
+        binaryBodyMode: true,
+        engine: "webview",
+        argument: scriptParams,
+      },
+    ],
+    mitm: {
+      hostname: ["weatherkit.apple.com"],
+    },
+  },
+});
