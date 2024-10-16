ab48f14ad00550f010bbfce380319a7363dc097d
Virgil Clyne <Virgil@nanocat.me>
2024-10-16 16:00:49 +0800

    build: rspack
    Update dev.yml

---
+++ rspack.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "@rspack/cli";
+import rspack from "@rspack/core";
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
+import pkg from "./package.json" with { type: "json" };
+
+export default defineConfig({
+	entry: {
+		"response": "./src/response.js",
+	},
+	output: {
+		filename: "[name].bundle.js",
+	},
+	plugins: [
+		new NodePolyfillPlugin({
+			//additionalAliases: ['console'],
+		}),
+		new rspack.BannerPlugin({
+			banner: `console.log('version: ${pkg.version}');`,
+			raw: true,
+		}),
+		new rspack.BannerPlugin({
+			banner: "console.log('[file]');",
+			raw: true,
+		}),
+		new rspack.BannerPlugin({
+			banner: "console.log(' iRingo: 🌤 WeatherKit');",
+			raw: true,
+		}),
+		new rspack.BannerPlugin({
+			banner: "https://NSRingo.github.io",
+		}),
+	],
+	performance: false,
+});
