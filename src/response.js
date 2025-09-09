39432d8eab5fca22aa3a6578b182b0d9b9b34402
脆薯饼 <Virgil@nanocat.me>
2025-09-09 10:49:08 +0800

    fix(HistoricalAirQuality): 注释掉历史空气质量数据的标准设置逻辑
    Update response.dev.js
    Update response.js

--- src/response.js
+++ src/response.js
@@ -221,7 +221,7 @@ async function HistoricalAirQuality(airQuality, Settings, enviroments) {
 		case "ColorfulClouds": {
 			historicalAirQuality = (await enviroments.colorfulClouds.Hourly(1, ((Date.now() - 864e5) / 1000) | 0)).airQuality;
 			// 因为彩云天气提供双标准 AQI，所以这里要保持与当前地区数据相同的标准，以处理 Settings?.AQI?.Local?.Scale === "NONE" 的回退情况
-			historicalAirQuality.scale = airQuality.scale;
+			//historicalAirQuality.scale = airQuality.scale;
 			break;
 		}
 		case "WAQI": {
