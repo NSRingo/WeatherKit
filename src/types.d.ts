76e4fb4b041d18aad430ae5f7bbee686fe8d5987
WordlessEcho <wordless@echo.moe>
2026-01-30 12:55:25 +0800

    feat: format types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -22,7 +22,7 @@ export interface Settings {
      */
     DataSets?: ('airQuality' | 'currentWeather' | 'forecastDaily' | 'forecastHourly' | 'forecastNextHour' | 'locationInfo' | 'news' | 'historicalComparisons' | 'weatherAlerts' | 'weatherChanges')[];
     Weather?: {
-    /**
+        /**
          * [天气] 数据源
          *
          * 始终会使用选定的数据源，替换天气数据。
@@ -37,9 +37,9 @@ export interface Settings {
          * @defaultValue "WeatherKit"
          */
         Provider?: 'WeatherKit' | 'ColorfulClouds' | 'QWeather';
-};
+    };
     NextHour?: {
-    /**
+        /**
          * [未来一小时降水强度] 数据源
          *
          * 始终会使用选定的数据源，填补无降水监测地区的数据。
@@ -54,9 +54,9 @@ export interface Settings {
          * @defaultValue "ColorfulClouds"
          */
         Provider?: 'WeatherKit' | 'ColorfulClouds' | 'QWeather';
-};
+    };
     AQI?: {
-    /**
+        /**
          * [空气质量] 数据源
          *
          * 始终会使用选定的数据源，填补无空气质量监测地区的数据。
@@ -72,7 +72,7 @@ export interface Settings {
          * @defaultValue "ColorfulClouds"
          */
         Provider?: 'WeatherKit' | 'ColorfulClouds' | 'QWeather' | 'WAQI';
-    /**
+        /**
          * [空气质量] 需要替换的供应商
          *
          * 选中的空气质量数据源会被替换。
@@ -87,7 +87,7 @@ export interface Settings {
          * @defaultValue ["QWeather"]
          */
         ReplaceProviders?: ('QWeather' | 'BreezoMeter' | 'TWC')[];
-    /**
+        /**
          * [空气质量] 对比昨日数据源
          *
          * 始终会使用选定的数据源，填补无对比昨日地区的数据。
@@ -104,86 +104,86 @@ export interface Settings {
          * @defaultValue "Auto"
          */
         ComparisonProvider?: 'Auto' | 'WeatherKit' | 'ColorfulClouds' | 'QWeather' | 'WAQI';
-    Local?: {
-        /**
-         * [空气质量] 需要修改的标准
-         *
-         * 选中的空气质量标准会被替换。请注意各国监测的污染物种类可能有所不同，转换算法或API未必适合当地。
-         *
-         * @remarks
-         *
-         * Possible values:
-         * - `'HJ6332012'` - 中国 (HJ 633—2012)
-         * - `'EPA_NowCast'` - 美国 (EPA NowCast)
-         *
-         * @defaultValue ["HJ6332012"]
-         */
-        ReplaceScales?: ('HJ6332012' | 'EPA_NowCast')[];
-        /**
-         * [空气质量] 本地替换算法
-         *
-         * 本地替换时使用的算法
-         *
-         * @remarks
-         *
-         * Possible values:
-         * - `'NONE'` - None (不进行替换)
-         * - `'EPA_NowCast'` - 美国 (EPA NowCast)
-         * - `'WAQI_InstantCast'` - WAQI InstantCast
-         *
-         * @defaultValue "EPA_NowCast"
-         */
-        Scale?: 'NONE' | 'EPA_NowCast' | 'WAQI_InstantCast';
-        /**
-         * [空气质量] 转换污染物计量单位
-         *
-         * 将污染物数据替换为转换单位后的数据，方便对照转换后的标准。（不推荐。不同单位互转可能会损失精度，导致数值偏大）
-         *
-         * @defaultValue false
-         */
-        ConvertUnits?: boolean;
-};
-};
-    API?: {
-    ColorfulClouds?: {
+        Local?: {
             /**
-         * [API] 彩云天气令牌
-         *
-         * 彩云天气 API 令牌
-         *
-         * @defaultValue ""
-         */
-        Token?: string;
-};
-    QWeather?: {
-        /**
-         * [API] 和风天气主机
-         *
-         * 和风天气 API 使用的主机名
-         *
-         * @defaultValue "devapi.qweather.com"
-         */
-        Host?: string;
-        /**
-         * [API] 和风天气令牌
-         *
-         * 和风天气 API 令牌
-         *
-         * @defaultValue ""
-         */
-        Token?: string;
-};
-    WAQI?: {
-        /**
-         * [API] WAQI 令牌
-         *
-         * WAQI API 令牌，填写此字段将自动使用WAQI高级API
-         *
-         * @defaultValue ""
-         */
-        Token?: string;
-};
-};
+             * [空气质量] 需要修改的标准
+             *
+             * 选中的空气质量标准会被替换。请注意各国监测的污染物种类可能有所不同，转换算法或API未必适合当地。
+             *
+             * @remarks
+             *
+             * Possible values:
+             * - `'HJ6332012'` - 中国 (HJ 633—2012)
+             * - `'EPA_NowCast'` - 美国 (EPA NowCast)
+             *
+             * @defaultValue ["HJ6332012"]
+             */
+            ReplaceScales?: ('HJ6332012' | 'EPA_NowCast')[];
+            /**
+             * [空气质量] 本地替换算法
+             *
+             * 本地替换时使用的算法
+             *
+             * @remarks
+             *
+             * Possible values:
+             * - `'NONE'` - None (不进行替换)
+             * - `'EPA_NowCast'` - 美国 (EPA NowCast)
+             * - `'WAQI_InstantCast'` - WAQI InstantCast
+             *
+             * @defaultValue "EPA_NowCast"
+             */
+            Scale?: 'NONE' | 'EPA_NowCast' | 'WAQI_InstantCast';
+            /**
+             * [空气质量] 转换污染物计量单位
+             *
+             * 将污染物数据替换为转换单位后的数据，方便对照转换后的标准。（不推荐。不同单位互转可能会损失精度，导致数值偏大）
+             *
+             * @defaultValue false
+             */
+            ConvertUnits?: boolean;
+        };
+    };
+    API?: {
+        ColorfulClouds?: {
+            /**
+             * [API] 彩云天气令牌
+             *
+             * 彩云天气 API 令牌
+             *
+             * @defaultValue ""
+             */
+            Token?: string;
+        };
+        QWeather?: {
+            /**
+             * [API] 和风天气主机
+             *
+             * 和风天气 API 使用的主机名
+             *
+             * @defaultValue "devapi.qweather.com"
+             */
+            Host?: string;
+            /**
+             * [API] 和风天气令牌
+             *
+             * 和风天气 API 令牌
+             *
+             * @defaultValue ""
+             */
+            Token?: string;
+        };
+        WAQI?: {
+            /**
+             * [API] WAQI 令牌
+             *
+             * WAQI API 令牌，填写此字段将自动使用WAQI高级API
+             *
+             * @defaultValue ""
+             */
+            Token?: string;
+        };
+    };
     /**
      * [调试] 日志等级
      *
